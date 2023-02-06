4ab85989c47b9eefe1e2e889ef85b5b68b87fc89
Nata <ganenkova@mail.ru>
2023-02-06 16:21:53 +0300

    Задание3

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите номер дня недели:");
+int num = int.Parse(Console.ReadLine());
+if(num>=1&& num<=5)
+{
+    Console.Write("Нет");
+}
+else if(num==6||num==7)
+{
+    Console.Write("Да");
+}   
+else
+{
+    Console.Write("Не существует такого дня недели");
+}  
