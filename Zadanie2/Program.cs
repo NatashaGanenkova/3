29ee0937f27ac8f2e5b1515d21262e70bb2d7e28
Nata <ganenkova@mail.ru>
2023-02-06 16:16:07 +0300

    Задание2

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите любое число: ");
+int a = Int32.Parse(Console.ReadLine());
+if(a <= 99){
+    Console.WriteLine("Число меньше 100");
+}else{
+    while(a > 1000){
+        a /=10; 
+
+    }
+        Console.WriteLine(a%10);
+}
