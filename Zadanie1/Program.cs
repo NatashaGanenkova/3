54b70255e3a4474346ced197eeb2955469358b6c
Nata <ganenkova@mail.ru>
2023-02-06 16:10:46 +0300

    Задание1

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите трехзначное число:");
+int num = int.Parse(Console.ReadLine());
+int num1 = num / 10;
+int result = num1 % 10;
+Console.WriteLine($"{result}");
